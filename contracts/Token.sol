7f3c76d9282aa47e0b18d14ffd384a5e9f13ee75
augustustung <huytung139@gmail.com>
2022-09-23 21:36:10 +0700

    initializing commit

---
+++ contracts/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply_ * 1 ether);
+    }
+
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount * 1 ether);
+    }
+}
