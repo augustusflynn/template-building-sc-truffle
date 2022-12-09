d3b08d64f5c5a24c360225eb8fa4cc9adbe0b277
augustustung <huytung139@gmail.com>
2022-12-09 09:21:38 +0700

    improving template

--- test/uint/example-test.js
+++ test/uint/example-test.js
@@ -1,5 +1,14 @@
-const Contract = artifacts.require("Contract")
-const { expect, assert } = require("chai")
+// require('@openzeppelin/test-helpers/configure')({
+//   provider: 'http://localhost:8080',
+// });
+const {
+  BN,           // Big Number support
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+  time
+} = require('@openzeppelin/test-helpers');
+const ERC20 = artifacts.require("ERC20")
 
 contract("Example Test", function (accounts) {
   let contractInstance
@@ -7,11 +16,44 @@ contract("Example Test", function (accounts) {
   const utils = web3.utils;
 
   before("should set up before test", async () => {
-    contractInstance = await Contract.deployed()
-    assert.notEqual(contractInstance.address, null);
-    assert.notEqual(contractInstance.address, undefined);
-    assert.notEqual(contractInstance.address, "0x0");
-    assert.notEqual(contractInstance.address, "");
+    this.value = new BN(1);
+    this.erc20 = await ERC20.new();
+    // BN assertions are automatically available via chai-bn (if using Chai)
+    assert.notEqual(this.erc20.address, null);
+    assert.notEqual(this.erc20.address, undefined);
+    assert.notEqual(this.erc20.address, "0x0");
+    assert.notEqual(this.erc20.address, "");
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
+    await time.advanceBlock();
+  });
+
+  it('reverts when transferring tokens to the zero address', async function () {
+    // Conditions that trigger a require statement can be precisely tested
+    await expectRevert(
+      this.erc20.transfer(constants.ZERO_ADDRESS, this.value, { from: accounts[0] }),
+      'ERC20: transfer to the zero address',
+    );
+  });
+
+  it('emits a Transfer event on successful transfers', async function () {
+    const receipt = await this.erc20.transfer(
+      accounts[1], this.value, { from: accounts[0] }
+    );
+
+    // Event assertions can verify that the arguments are the expected ones
+    expectEvent(receipt, 'Transfer', {
+      from: accounts[0],
+      to: accounts[1],
+      value: this.value,
+    });
+  });
+
+  it('updates balances on successful transfers', async function () {
+    this.erc20.transfer(accounts[1], this.value, { from: accounts[0] });
+
+    // BN assertions are automatically available via chai-bn (if using Chai)
+    expect(await this.erc20.balanceOf(accounts[1]))
+      .to.be.bignumber.equal(this.value);
   });
 
   it(".....", async () => {
